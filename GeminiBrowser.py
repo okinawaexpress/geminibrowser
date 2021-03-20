cb7f16b3cba26f6a61cdc575461eb1cf2b15e1f4
okinawaexpress <80932087+okinawaexpress@users.noreply.github.com>
2021-03-19 22:25:47 -0700

    Add files via upload

--- GeminiBrowser.py
+++ GeminiBrowser.py
@@ -1,73 +1,76 @@
 '''
 To-Do
-
-- Parse data recieved from server to display correctly
-  - Implement error code handling/MIME data type handling
-- Useable hyperlinks
-  - Assign each link on the page a number, user will type number to redirect
-- Make a data structure that keeps track of the current directory, as well as
-  previously visited pages.
-   - For indirect links, and for back/forward buttons on final browser
+- Switchable modes for user input (direct url entry, link number, forwards/backwards)
+- Handle Meta codes and formatting
+- Assign each link on the page a number, user will type number to redirect
+- Keep a log of pages/sites visted for a backwards/forwards feature
 '''
 
 import socket, ssl, re
 
-def getHostData(url):
+#Page browser is currently on
+currentPage = ''
 
-    #Regex/append to get host/urlRequest
-    
-    #Get hostname
-    #compile regex
-    protocolPattern = re.compile('://')
-    directoryPattern = re.compile('/')
+def parseURL(url):
+  #Should always be an absolute link
+  #gets hostname/urlRequest out of url given
 
-    #remove protocol
-    protocolEnd = protocolPattern.search(url)
-    protocolEndPoint = protocolEnd.end()
-    host = url[protocolEndPoint:]
+  #Get hostname
+  #compile regex
+  protocolPattern = re.compile('://')
+  directoryPattern = re.compile('/')
 
-    #remove directories
-    directoryStart = directoryPattern.search(host)
-    directoryStartPoint = directoryStart.start()
-    host = host[0:directoryStartPoint]
+  #remove protocol
+  protocolEnd = protocolPattern.search(url)
+  protocolEndPoint = protocolEnd.end()
+  host = url[protocolEndPoint:]
 
-    #Get urlRequest
-    urlRequest = url + '\r\n'
- 
-    #Binary data from host
-    binary = []
+  #remove directories
+  directoryStart = directoryPattern.search(host)
+  directoryStartPoint = directoryStart.start()
+  host = host[0:directoryStartPoint]
 
-    #Set up socket -> Wrap in TLS protocol
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    wrapped_socket = context.wrap_socket(s, server_hostname=host)
+  #Get urlRequest
+  urlRequest = url + '\r\n'
+  return host, urlRequest
 
-    #Connect to host, send the request
-    wrapped_socket.connect((host, 1965))
-    wrapped_socket.send(urlRequest.encode())
+def connectToHost(connectionInfo):
 
-    #Recieve data in 2048 byte chunks
-    while True:
-        data = wrapped_socket.recv(2048)
-        data = data.decode()
-        binary.append(data)
-        if (len(data) < 1):
-            break
+  #Connects to host, recieves data, returns header (error code/MIME type) and response body
 
-    #Compound the entire response body together
-    headerInfo = binary.pop(0)
-    binaryData = ''.join(binary)
+  binary = []
 
-    #Close socket, return data
-    wrapped_socket.close()
-    return headerInfo, binaryData
-    print("closed")
+  #Set up socket -> Wrap in TLS protocol
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+  wrapped_socket = context.wrap_socket(s, server_hostname=connectionInfo[0])
 
-#rly quick parser just seperates lines by \n breaks
-#(according to gemini standard)
+  #Connect to host, send the request
+  wrapped_socket.connect((connectionInfo[0], 1965))
+  wrapped_socket.send(connectionInfo[1].encode())
+
+  #Recieve data in 2048 byte chunks
+  while True:
+    data = wrapped_socket.recv(2048)
+    data = data.decode()
+    binary.append(data)
+    if (len(data) < 1):
+      break
+
+  #Compound the entire response body together
+  headerInfo = binary.pop(0)
+  responseInfo = ''.join(binary)
+
+  #Close socket, return data
+  wrapped_socket.close()
+  return headerInfo, responseInfo
+  print("closed")
+
 def parseData(data):
-    #Check Error Codes first
+    print(data[0])
+    #Check status Codes first
     #Check MIME type
+    #parse/display data (including link handling)
     newdata = data[1].split('\n')
     for i in newdata:
         if len(i) < 1:
@@ -75,7 +78,17 @@ def parseData(data):
         else:
             print(i)
 
-#Main Loop
 while True:
     url = input("> ")
-    parseData(getHostData(url))
+    if '://' and 'gemini://' in url:
+      #absolute url
+      currentPage = url
+      parseData(connectToHost(parseURL(url)))
+    elif '://' in url:
+      #different protocol (refuse)
+      print("Different protocol, connection refused")
+    else:
+      #relative url
+      currentPage = currentPage + url
+      url = currentPage
+      parseData(connectToHost(parseURL(url)))
